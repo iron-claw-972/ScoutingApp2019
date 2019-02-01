40486056dcb48366f0dde1b73adeeb280124bae2
Pranav <orca.pranav@gmail.com>
2019-01-31 22:49:01 -0800

    more stuff

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-import app.pit.*
-import app.scouting.*
-import app.publicity.*
+import app.pit.batteryTracker
+import app.pit.liveStream
+import app.pit.matchData
+import app.pit.matchSchedule
+import app.scouting.matchScouting
+import app.scouting.pitScouting
+import app.scouting.templatingData
+import app.publicity.publicity
