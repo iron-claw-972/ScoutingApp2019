a5824f10480fe08b53f11f647eb768d89e263c3d
Pranav <orca.pranav@gmail.com>
2019-02-02 11:10:06 -0800

    more stuff

--- app/scouting/matchScouting.py
+++ app/scouting/matchScouting.py
@@ -4,17 +4,14 @@ import sys
 sys.path.append("..")
 # print(app.app)
 
-"""
+
 @app.route('/scouting/match', methods=['GET', 'POST'])
 def matchScouting():
+    if request.method == "GET":
+        return '<html><body><form action="/scouting/match" method="POST"><input type="text" name="fname"><input type="submit" value="Submit"></form></body></html>'
     if request.method == "POST":
         fields = [k for k in request.form]
         values = [request.form[k] for k in request.form]
         data = dict(zip(fields, values))
-    return jsonify(data)
-"""
-
-
-@app.route("/", methods=['GET'])
-def test():
-    return "it works"
+        print(data)
+    return str(data)
--- app/scouting/pitScouting.py
+++ app/scouting/pitScouting.py
@@ -1,4 +1,17 @@
+from app import app
+from flask import Flask, request
 import sys
 sys.path.append("..")
-import app
-print(app.app)
+# print(app.app)
+
+
+@app.route('/scouting/pit', methods=['GET', 'POST'])
+def matchScouting():
+    if request.method == "GET":
+        return '<html><body><form action="/scouting/pit" method="POST"><input type="text" name="fname"><input type="submit" value="Submit"></form></body></html>'
+    if request.method == "POST":
+        fields = [k for k in request.form]
+        values = [request.form[k] for k in request.form]
+        data = dict(zip(fields, values))
+        print(data)
+    return str(data)
