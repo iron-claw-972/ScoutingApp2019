74cc85e25d8d3d66cf4269cccadf4637e49c261e
Val <valeriy.rotan@gmail.com>
2019-02-02 23:33:33 -0800

    configured debug mode

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_ci import CICommand
 from tests import settings
 import time
 app = Flask(__name__)
+
+app.debug = True
 
 # import pit.liveStream
 #import pit.matchData
@@ -15,9 +17,9 @@ import scouting.matchScouting as matchScouting
 import scouting.pitScouting as pitScouting
 SCOUTING_PREFIX = '/scouting'
 
-app.register_blueprint(data.bp, url_prefix = '/scouting')
-app.register_blueprint(matchScouting.bp, url_prefix = '/scouting')
-app.register_blueprint(pitScouting.bp, url_prefix = '/scouting')
+app.register_blueprint(data.bp, url_prefix = SCOUTING_PREFIX)
+app.register_blueprint(matchScouting.bp, url_prefix = SCOUTING_PREFIX)
+app.register_blueprint(pitScouting.bp, url_prefix = SCOUTING_PREFIX)
 
 
 
