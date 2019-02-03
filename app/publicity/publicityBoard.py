694cca7dc9973a52d389ae93a88319021278a247
Val <valeriy.rotan@gmail.com>
2019-02-02 23:55:52 -0800

    started publicity board

--- app/publicity/publicityBoard.py
+++ app/publicity/publicityBoard.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, Blueprint
+import flask
 
-bp = Blueprint('publicityBoard', __name__)
+bp = Blueprint('publicityBoard', __name__, template_folder='templates')
 
 @bp.route('/publicityBoard', methods=['GET'])
 def handle():
-    return 'publicityBoard'
+    return flask.render_template('publicityBoard.html')
