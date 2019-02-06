70675822cb6e7b8331f16700732b401d76487fbd
Pranav <orca.pranav@gmail.com>
2019-02-05 16:12:31 -0800

    api key

---
+++ app/util/DataScraper.py
@@ -0,0 +1,7 @@
+import requests
+
+
+class DataScraper:
+    baseUrl = "https://www.thebluealliance.com/api/v3"
+    apiKey = "1CQGBNHADuOcI5xDHmLjpIBdQwxTHzZmPpDohm1gNs79gDtN5BrGgIt1bPzAlx2N"
+    SVReventKey = ""
