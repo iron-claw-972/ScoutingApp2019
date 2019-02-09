96dc439b88a2b1c5849ceeb74210c2f914117480
Pranav <orca.pranav@gmail.com>
2019-02-08 18:29:37 -0800

    Update DataScraper.py

--- app/util/DataScraper.py
+++ app/util/DataScraper.py
@@ -1,18 +1,18 @@
 import requests
 import json
+import tbapy
 
 
 class DataScraper:
     baseUrl = "https://www.thebluealliance.com/api/v3"
     apiKey = "1CQGBNHADuOcI5xDHmLjpIBdQwxTHzZmPpDohm1gNs79gDtN5BrGgIt1bPzAlx2N"
+    tba = tbapy.TBA(apiKey)
     SVReventKey = ""
     SFeventKey = ""
 
     def __init__(self):
-        self.SVReventKey = self.getEvent("2018", "Silicon Valley Regional")[
-            'first_event_id']
-        self.SFeventKey = self.getEvent("2018", "San Francisco Regional")[
-            'first_event_id']
+        self.SVReventKey = '2018casj'
+        self.SFeventKey = '2018casf'
 
     def getEvent(self, year, name):
         events = json.loads(requests.get(self.baseUrl + "/events/" +
@@ -23,6 +23,22 @@ class DataScraper:
         return json.loads(requests.get(self.baseUrl + "/team/frc"+team+"/events/" +
                                        year+"/statuses" + '?X-TBA-Auth-Key='+self.apiKey).text)
 
+    def getTeams(self, year, id):
+        return json.loads(requests.get(self.baseUrl + "/event/" + id + "/teams" + '?X-TBA-Auth-Key=' + self.apiKey).text)
 
-print(DataScraper().getEvent("2018", "San Francisco Regional"))
-print(DataScraper().getTeamInfo("254", "2018"))
+    def getTeamEventStatus(self, team, event_key):
+        return json.loads(requests.get(self.baseUrl + '/team/frc'+team+'/event/'+event_key+'/status'+'?X-TBA-Auth-Key='+self.apiKey).text)
+
+    def eventAlliances(self, event_key):
+        return json.loads(requests.get(self.baseUrl + '/event/'+event_key+'/alliances'+'?X-TBA-Auth-Key='+self.apiKey).text)
+
+    def getTeamMatches(self, team, event_key):
+        return json.loads(requests.get(self.baseUrl + '/team/frc'+team+'/event/'+event_key+'/matches'+'?X-TBA-Auth-Key='+self.apiKey).text)
+
+
+datascraper = DataScraper()
+# print(datascraper.getEvent("2019", "San Francisco Regional"))
+# print(datascraper.getTeamInfo("254", "2018"))
+print(datascraper.eventAlliances(datascraper.SFeventKey))
+# print(datascraper.SVReventKey)
+# print(datascraper.getTeams("2019", datascraper.SFeventKey))
