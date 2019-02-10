ae6456a1a65157b43699e6e37182eb2c3455a3c4
me1234q <me1234q@gmail.com>
2019-02-10 09:19:24 -0800

    deleted comments on tbapy

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 import publicity.publicityBoard as publicityBoard
 import scouting.scoutingHandler as scoutingHandler
 import pit.pitHandler as pitHandler
-#import util.DatabaseUtil as database
-#import util.DataScraper as scraper
+import util.DatabaseUtil as database
+import util.DataScraper as scraper
 from flask import Flask
 
 app = Flask(__name__, static_folder="static", static_url_path="")
@@ -13,10 +13,10 @@ PIT_PREFIX = '/pit'
 SCOUTING_PREFIX = '/scouting'
 
 #  config, evntually move to config
-#app.database = database.DatabaseUtil
-#app.database.storeVariable('batteryStatus', ['good', 'good', 'good'])
-#app.database.storeVariable('timeUntilGood', [0, 0, 0])
-#app.database.storeVariable('batteryChargingTime', 5)
+app.database = database.DatabaseUtil
+app.database.storeVariable('batteryStatus', ['good', 'good', 'good'])
+app.database.storeVariable('timeUntilGood', [0, 0, 0])
+app.database.storeVariable('batteryChargingTime', 5)
 
 app.register_blueprint(pitHandler.bp, url_prefix=PIT_PREFIX)
 app.register_blueprint(scoutingHandler.bp, url_prefix=SCOUTING_PREFIX)
