9f3c0733d0f856523a0e7e27afb3379f5514beb5
me1234q <me1234q@gmail.com>
2019-02-09 23:04:22 -0800

    changed logos

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import publicity.publicityBoard as publicityBoard
 import scouting.scoutingHandler as scoutingHandler
 import pit.pitHandler as pitHandler
-import util.DatabaseUtil as database
+#import util.DatabaseUtil as database
 #import util.DataScraper as scraper
 from flask import Flask
 
@@ -13,10 +13,10 @@ PIT_PREFIX = '/pit'
 SCOUTING_PREFIX = '/scouting'
 
 #  config, evntually move to config
-app.database = database.DatabaseUtil
-app.database.storeVariable('batteryStatus', ['good', 'good', 'good'])
-app.database.storeVariable('timeUntilGood', [0, 0, 0])
-app.database.storeVariable('batteryChargingTime', 5)
+#app.database = database.DatabaseUtil
+#app.database.storeVariable('batteryStatus', ['good', 'good', 'good'])
+#app.database.storeVariable('timeUntilGood', [0, 0, 0])
+#app.database.storeVariable('batteryChargingTime', 5)
 
 app.register_blueprint(pitHandler.bp, url_prefix=PIT_PREFIX)
 app.register_blueprint(scoutingHandler.bp, url_prefix=SCOUTING_PREFIX)
