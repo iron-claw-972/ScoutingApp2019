717075aea4830f0cb4ebf864d65e9867f9f0b2d8
Pranav <orca.pranav@gmail.com>
2019-02-09 16:21:39 -0800

    added 404 and team pages

--- app/util/DatabaseUtil.py
+++ app/util/DatabaseUtil.py
@@ -1,6 +1,9 @@
+from .DataScraper import datascraper
+
 
 class DatabaseUtil:
     variableStorage = {}
+    teamData = {"972": {}}
 
     @staticmethod
     def storeVariable(name, value):
@@ -9,3 +12,23 @@ class DatabaseUtil:
     @staticmethod
     def getVariable(name):
         return DatabaseUtil.variableStorage[name]
+
+    @staticmethod
+    def writeToTeam(number, attribute, value):
+        DatabaseUtil.teamData[str(number)][attribute] = value
+
+    @staticmethod
+    def getTeamData(number):
+        return DatabaseUtil.teamData[str(number)]
+
+    @staticmethod
+    def addBATeamData(number):
+        DatabaseUtil.teamData[number] = {}
+        DatabaseUtil.teamData[number]['compResults'] = datascraper.getTeamInfo(
+            number, '2018')
+        DatabaseUtil.teamData[number]['teamInfo'] = datascraper.getSecretTeamInfo(
+            number)
+
+
+DatabaseUtil.addBATeamData('972')
+print(DatabaseUtil.getTeamData("972"))
