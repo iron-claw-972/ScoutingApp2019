c5084289b0f2e6491b4b5c433cf87a00e2a3c596
Pranav <orca.pranav@gmail.com>
2019-02-15 16:36:15 -0800

    Update DatabaseUtil.py
    stuff

--- app/util/DatabaseUtil.py
+++ app/util/DatabaseUtil.py
@@ -13,12 +13,22 @@ class DatabaseUtil:
         database="app_test"
     )
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM new_table")
+    mycursor.execute("SELECT * FROM team_info")
     print([e for e in mycursor.fetchall()])
 
     @staticmethod
     def storeVariable(name, value):
         DatabaseUtil.variableStorage[name] = value
+
+    @staticmethod
+    def addTeam(dictOfValues):
+        keys = [e for e in dictOfValues]
+        print('''INSERT INTO team_info('''+','.join([e for e in keys])+''')
+                         VALUES('''+','.join(["'"+dictOfValues[key]+"'" for key in keys])+''')
+                         ''')
+        DatabaseUtil.mycursor.execute('''INSERT INTO team_info('''+','.join([e for e in keys])+''')
+                         VALUES('''+','.join(["'"+dictOfValues[key]+"'" for key in keys])+''')
+                         ''')
 
     @staticmethod
     def getVariable(name):
@@ -41,5 +51,7 @@ class DatabaseUtil:
             number)
 
 
-DatabaseUtil.addBATeamData('972')
-print(DatabaseUtil.getTeamData("972"))
+# DatabaseUtil.addBATeamData('972')
+# print(DatabaseUtil.getTeamData("972"))
+DatabaseUtil.addTeam(
+    {"Name": "Iron Claw Robotics", "TeamNumber": "973"})
