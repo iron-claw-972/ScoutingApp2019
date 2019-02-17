b5d1f6555e3d7fb7056d86c3d64e358346cef634
me1234q <me1234q.me1234q@gmail.com>
2019-02-16 18:00:10 -0800

    added basic match scouting

--- app/scouting/matchScouting.py
+++ app/scouting/matchScouting.py
@@ -3,10 +3,19 @@ import flask
 
 def matchScouting(request):
     if request.method == "GET":
-        return flask.render_template('matchScouting.html')
-    if request.method == "POST":
+        return flask.render_template('/matchScouting/inputMatchNumber.html')
+    elif request.method == "POST":
         fields = [k for k in request.form]
         values = [request.form[k] for k in request.form]
+        
         data = dict(zip(fields, values))
         print(data)
+
+        if('teamNumber' in fields):
+            return flask.render_template('/matchScouting/matchScouting.html', matchNumber=request.form['matchNumber'], teamNumber=request.form['teamNumber'])
+        if('matchNumber' in fields):
+            return flask.render_template('/matchScouting/inputTeamNumber.html', matchNumber=request.form['matchNumber'])
+        
+
+
     return str(data)
