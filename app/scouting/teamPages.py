f236d782f484fab981fdb7c39907351c35f6c2c4
Pranav <orca.pranav@gmail.com>
2019-02-16 19:58:45 -0800

    bug fixes

--- app/scouting/teamPages.py
+++ app/scouting/teamPages.py
@@ -11,16 +11,12 @@ def teamPages(number):
             return teamPages(str(randint(1, 3790)))
     else:
         database = current_app._get_current_object().database
-        try:
-            data = database.getTeamData(number)
-        except:
-            database.addBATeamData(number)
-            data = database.getTeamData(number)
+        database.addBATeamData(number)
+        data = database.getTeamData(number)
         try:
             name = data['teamInfo']['nickname']
             website = data['teamInfo']['website']
         except:
+            return str(data)
             return flask.render_template('404.html')
         return flask.render_template('teamPages.html', num=str(number), name=data['teamInfo']['nickname'], website=website)
-    
-    
