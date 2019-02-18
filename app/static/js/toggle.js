d5513f9c1ca8ae35d55f7f15175a00bd82e7cf1a
me1234q <me1234q@gmail.com>
2019-02-18 13:02:06 -0800

    added toggle button

---
+++ app/static/js/toggle.js
@@ -0,0 +1,4 @@
+function toggle() {
+    document.getElementById("background").style.backgroundImage = "url(\"../images/scoutingLogo.png\")";
+    document.getElementById("toggle").style.display = "none";
+}
