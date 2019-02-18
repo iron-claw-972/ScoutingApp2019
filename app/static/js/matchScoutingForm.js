19725484c2c7ece0fd129b1788fa3b4b4b858694
Pranav <orca.pranav@gmail.com>
2019-02-18 14:20:14 -0800

    adding counters

---
+++ app/static/js/matchScoutingForm.js
@@ -0,0 +1,11 @@
+inputid = ['cargo0', 'cargo1', 'cargo2', 'cargo3', 'hatch0', 'hatch1', 'hatch2', 'hatch3']
+labelid = ['c0', 'c1', 'c2', 'c3', 'h0', 'h1', 'h2', 'h3']
+var element
+for (var i = 0; i < inputid.length; i++) {
+    element = document.getElementById(inputid[i])
+    element.idnum = i;
+    element.onclick = function () {
+        var increase = document.getElementById(labelid[this.idnum]);
+        increase.textContent = Number(increase.textContent) + 1
+    }
+}
