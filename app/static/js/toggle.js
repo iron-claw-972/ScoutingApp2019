3c4f322352b9dd14f77bae173d057ac979a5796a
me1234q <me1234q.me1234q@gmail.com>
2019-02-25 17:55:44 -0800

    added pit scouting stuff

--- app/static/js/toggle.js
+++ app/static/js/toggle.js
@@ -1,4 +1,4 @@
 function toggle() {
-    document.getElementById("background").style.backgroundImage = "url(\"../images/scoutingLogo.png\")";
+    document.getElementById("background").style.backgroundImage = "url(\"../images/toggle/scoutingLogo.png\")";
     document.getElementById("toggle").style.display = "none";
 }
