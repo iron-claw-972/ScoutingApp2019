36f4b8a6df5e04a36c2b6c156221bdd6585346c6
me1234q <me1234q.me1234q@gmail.com>
2019-03-05 16:27:52 -0800

    image uploadin

--- app/app.py
+++ app/app.py
@@ -5,7 +5,9 @@ import pit.pitHandler as pitHandler
 import util.DatabaseUtil as database
 import util.DataScraper as scraper
 
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_folder="static", static_url_path="")
 app.secret_key = 'no lookey lookey at me nicey long key so you are no gooooooooooood dont be him'
@@ -13,6 +15,14 @@ app.debug = True
 
 PIT_PREFIX = '/pit'
 SCOUTING_PREFIX = '/scouting'
+
+UPLOAD_FOLDER = 'static/images/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #  config, evntually move to config
 app.database = database.DatabaseUtil
--- app/scouting/pitScouting.py
+++ app/scouting/pitScouting.py
@@ -24,6 +24,21 @@ def pitScouting(request):
         hatchOutake = ('C' if badata['hatchLevelC'] == 'on' else '') + ('1' if badata['hatchLevel1'] == 'on' else '') + (
             '2' if badata['hatchLevel2'] == 'on' else '') + ('3' if badata['hatchLevel3'] == 'on' else '')
 
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
         database.addTeam({'TeamNumber': badata['teamNumber'], 'HatchLevels': hatchOutake, 'CargoLevels': cargoOutake, 'HatchIntake': hatchIntake, 'CargoComments': badata['cargoComments'], 'CargoIntake': cargoIntake, 'HatchComments': badata['hatchComments'],
                           'DriveTrain': badata['driveTrain'], 'ClimbLevels': badata['climbLevel'], 'CycleTime': badata['cycleTime'], 'Weight': badata['weight'], 'ProgrammingLanguage': badata['ProgramLang'], 'Comments': badata['comments']})
         print(badata)
