116d47c287c9e02ee528a25864312d05809dd6ef
Pranav <orca.pranav@gmail.com>
2019-03-09 23:22:07 -0800

    added countdown

--- app/pit/matchSchedule.py
+++ app/pit/matchSchedule.py
@@ -3,5 +3,22 @@ import time
 
 
 def matchSchedule(scraper):
-    currentTime = time.time()
-    results = scraper.getMatches(scraper.SFeventKey, currentTime)
+    # currentTime = time.time()
+    currentTime = 1521323100
+    scraper = scraper.DataScraper()
+    results = scraper.getMatches(scraper.SFeventKey, currentTime)[0:2]
+    a1 = results[0]
+    a2 = results[1]
+    AP1 = ', '.join([e for e in (a1['alliances']['red']['team_keys'] if 'frc972' in a1['alliances']
+                                 ['red']['team_keys'] else a1['alliances']['blue']['team_keys']) if e != 'frc972'])
+
+    OT1 = ', '.join([e for e in (a1['alliances']['red']['team_keys'] if 'frc972' not in a1['alliances'][
+        'red']['team_keys'] else a1['alliances']['blue']['team_keys']) if e != 'frc972'])
+    T1 = str(a1['time']-currentTime)
+
+    AP2 = ', '.join([e for e in (a2['alliances']['red']['team_keys'] if 'frc972' in a2['alliances'][
+        'red']['team_keys'] else a2['alliances']['blue']['team_keys']) if e != 'frc972'])
+    OT2 = ', '.join([e for e in (a2['alliances']['red']['team_keys'] if 'frc972' not in a2['alliances'][
+        'red']['team_keys'] else a2['alliances']['blue']['team_keys']) if e != 'frc972'])
+    T2 = str(a2['time']-currentTime)
+    return AP1, OT1, T1, AP2, OT2, T2
