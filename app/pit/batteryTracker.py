55365b9d3e06ac07cdc7e4e28ee2d60101a18bf4
Pranav <orca.pranav@gmail.com>
2019-03-11 16:13:58 -0700

    stuff

--- app/pit/batteryTracker.py
+++ app/pit/batteryTracker.py
@@ -8,15 +8,16 @@ def batteryTracker(database, request):
         ) else "bad" for it in database.getVariable('timeUntilGood')])
         return database.getVariable('batteryStatus')[0], database.getVariable('batteryStatus')[1], database.getVariable('batteryStatus')[2]
     if request.method == "POST":
+        print(request.form)
         if request.form["state"] in list('123'):
             batteryStatus = database.getVariable("batteryStatus")
             batteryStatus[int(request.form["state"])-1] = "bad"
             database.storeVariable('batteryStatus', batteryStatus)
-
             timeUntilGood = database.getVariable('timeUntilGood')
             batteryChargingTime = database.getVariable("batteryChargingTime")
 
             timeUntilGood[int(request.form["state"])-1
                           ] = time.time()+batteryChargingTime
             database.storeVariable('batteryStatus', batteryStatus)
+
         return database.getVariable('batteryStatus')[0], database.getVariable('batteryStatus')[1], database.getVariable('batteryStatus')[2]
